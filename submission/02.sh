39887dbb9f52e7296adb89ae4b23335a64635e37
Yussif Muniru <yussifmuniruwulon@gmail.com>
2025-05-03 13:39:21 +0000

    Fixed 02.sh hardcoded output count values

--- submission/02.sh
+++ submission/02.sh
@@ -2,7 +2,14 @@
 
 #!/bin/bash
 
-OUTPUT_COUNT=442
+# OUTPUT_COUNT=442
+
+# echo $OUTPUT_COUNT
+
+
+BLOCKHASH=$(bitcoin-cli -signet getblockhash 243825)
+
+OUTPUT_COUNT=$(bitcoin-cli -signet getblock $BLOCKHASH 2 | jq '[.tx[].vout | length] | add')
+
 
 echo $OUTPUT_COUNT
-
